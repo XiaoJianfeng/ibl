a437a4d6f04638b9e421e9283e8a2383d7651bc3
XiaoJianfeng <xiaojianfeng2013@gmail.com>
2014-08-11 12:20:01 +0800

    deal with multipe match correctly. support gzip.
    fixed a bug where multiple alignments for a read are not correctly processed.
    the output is compressed with gzip.

--- util/bam2fq.sh
+++ util/bam2fq.sh
@@ -3,7 +3,8 @@
 #
 # Author: Xiao Jianfeng
 #
-# Date:   2014.08.05
+# last modified: 2014.08.11
+# Date:          2014.08.05
 
 if [ $# -eq 0 -o "$1" == "-h" -o "$1" == "--help" ]
 then
@@ -23,15 +24,27 @@ fi
 
 # bam_file=WGC021367D.mapped.bam
 
-left_file=${prefix}_1.fq
-right_file=${prefix}_2.fq
+left_file=${prefix}_1.fq.gz
+right_file=${prefix}_2.fq.gz
+left_file_tmp=tmp.left.$$
+right_file_tmp=tmp.right.$$
 
-temp_file=temp.$$
-mkfifo ${temp_file}
+mkfifo ${left_file_tmp}
+mkfifo ${right_file_tmp}
+gzip -c < ${left_file_tmp} > ${left_file} &
+gzip -c < ${right_file_tmp} > ${right_file} &
+
+tmp_file=tmp.$$
+mkfifo ${tmp_file}
 
 # for test, use 'head -n 10000' to limit the input
-# samtools view ${bam_file} | cut -f 1,2,10,11 | head -n 2000000 > ${temp_file} &
-samtools view ${bam_file} | cut -f 1,2,10,11 > ${temp_file} &
+# samtools view ${bam_file} | cut -f 1,2,10,11 | head -n 2000000 > ${tmp_file} &
+samtools view ${bam_file} | cut -f 1,2,10,11 > ${tmp_file} &
+
+# the primary alignment should have flag & 0x900 == 0, first read has flag & 0x40 == 0x40, and second read has flag & 0x80 == 0x80
+# so primary alignment of first read should be flag & 0x940 (ie. 2368) == 0x40, and for second read flag & 0x980 (ie. 2432) == 0x80
+# CREATE TABLE left as select qname from fastq where flag & 2368 == 64;
+# CREATE TABLE right as select qname from fastq where flag & 2432 == 128;
 
 sqlite3 <<-EOF
 .output stderr
@@ -43,13 +56,13 @@ CREATE TABLE fastq (qname, flag, seq, qual);
 
 select date()||" "||time()||" import begin";
 .separator "\t"
-.import temp.$$ fastq
+.import tmp.$$ fastq
 CREATE INDEX qname_idx on fastq (qname);
 
 select date()||" "||time()||" import done";
 
-CREATE TABLE left as select qname from fastq where flag & 64 == 64;
-CREATE TABLE right as select qname from fastq where flag & 128 == 128;
+CREATE TABLE left as select qname from fastq where flag & 2368 == 64;
+CREATE TABLE right as select qname from fastq where flag & 2432 == 128;
 
 select date()||" "||time()||" create left right done";
 
@@ -58,19 +71,21 @@ CREATE INDEX right_idx on right (qname);
 
 select date()||" "||time()||" create left right index done";
 
-.output ${left_file}
+.output ${left_file_tmp}
 .separator "\n"
-select distinct "@"||right.qname||"/1", seq, '+', qual from right join fastq on fastq.qname == right.qname where flag&64 == 64 order by right.qname;
+select distinct "@"||fastq.qname||"/1", seq, '+', qual from right join fastq on fastq.qname == right.qname where flag&2368 == 64 order by right.qname;
 
 .output stderr
 select date()||" "||time()||" create left reads done";
 
-.output ${right_file}
+.output ${right_file_tmp}
 .separator "\n"
-select distinct "@"||left.qname||"/2", seq, '+', qual from left join fastq on fastq.qname == left.qname where flag&128 == 128 order by left.qname;
+select distinct "@"||fastq.qname||"/2", seq, '+', qual from left join fastq on fastq.qname == left.qname where flag&2432 == 128 order by left.qname;
 
 .output stderr
 select date()||" "||time()||" create right reads done";
 .exit
 EOF
-rm ${temp_file}
+
+wait
+rm ${tmp_file} ${left_file_tmp} ${right_file_tmp}
